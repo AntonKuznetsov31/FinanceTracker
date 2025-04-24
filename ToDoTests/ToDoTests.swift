5fd0b6715bdb2d0878559b3126571f4758a68bf1
Anton Kuznetsov <akuznetsov.appwell@ancestry.com>
2025-04-24 14:04:22 +0200

    Initial Commit

---
+++ ToDoTests/ToDoTests.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoTests.swift
+//  ToDoTests
+//
+//  Created by Anton Kuznetsov on 24/04/2025.
+//
+
+import Testing
+
+struct ToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
